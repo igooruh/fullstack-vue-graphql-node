51ec78300c55dc6657d5ec67b72ed7122a30579f
Igor Pedro L M Martins <igooruh@icloud.com>
2019-10-28 01:13:11 -0300

    Task 00008 - Indents after settings

--- src/App.vue
+++ src/App.vue
@@ -10,48 +10,48 @@
 
       <section class="template-view">
         <div class="container">
-          <div class="row">
-            <div class="col-md">
-              <h5>Prefixos <span class="badge badge-info">{{ prefixes.length }}</span></h5>
-              <div class="card">
-                <div class="card-body">
-                  <ul class="list-group">
-                    <li class="list-group-item" v-for="prefix in prefixes" v-bind:key="prefix">
-                      {{ prefix }}
-                    </li>
-                  </ul>
-                  <br />
-                  <input class="form-control" type="text" placeholder="Digite o prefixo " />
-                </div>
-              </div>
-            </div>
-            <div class="col-md">
-              <h5>Sufixos <span class="badge badge-info">{{ sufixes.length }}</span></h5>
-              <div class="card">
-                <div class="card-body">
-                  <ul class="list-group">
-                    <li class="list-group-item" v-for="sufix in sufixes" v-bind:key="sufix">
-                      {{ sufix }}
-                    </li>
-                  </ul>
-                  <br />
-                  <input class="form-control" type="text" placeholder="Digite o sufixo " />
-                </div>
-              </div>
-            </div>
-          </div>
+			<div class="row">
+				<div class="col-md">
+					<h5>Prefixos <span class="badge badge-info">{{ prefixes.length }}</span></h5>
+					<div class="card">
+						<div class="card-body">
+							<ul class="list-group">
+								<li class="list-group-item" v-for="prefix in prefixes" v-bind:key="prefix">
+									{{ prefix }}
+								</li>
+							</ul>
+							<br />
+							<input class="form-control" type="text" placeholder="Digite o prefixo " />
+						</div>
+					</div>
+				</div>
+				<div class="col-md">
+					<h5>Sufixos <span class="badge badge-info">{{ sufixes.length }}</span></h5>
+					<div class="card">
+						<div class="card-body">
+							<ul class="list-group">
+								<li class="list-group-item" v-for="sufix in sufixes" v-bind:key="sufix">
+									{{ sufix }}
+								</li>
+							</ul>
+							<br />
+							<input class="form-control" type="text" placeholder="Digite o sufixo " />
+						</div>
+					</div>
+				</div>
+			</div>
 
-          <br/>
-          <h5>Domínios <span class="badge badge-info">{{ domains.length }}</span></h5>
-          <div class="card">
-            <div class="card-body">
-              <ul class="list-group">
-                <li class="list-group-item" v-for="domain in domains" v-bind:key="domain">
-                  {{ domain.name }}
-                </li>
-              </ul>
-            </div>
-          </div>
+			<br/>
+			<h5>Domínios <span class="badge badge-info">{{ domains.length }}</span></h5>
+			<div class="card">
+				<div class="card-body">
+					<ul class="list-group">
+						<li class="list-group-item" v-for="domain in domains" v-bind:key="domain">
+							{{ domain.name }}
+						</li>
+					</ul>
+				</div>
+			</div>
         </div>
       </section>
     </div>
@@ -60,95 +60,95 @@
 </template>
 
 <script>
-import "bootstrap/dist/css/bootstrap.css"
-import "font-awesome/css/font-awesome.css"
+import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.css';
 
 export default {
-  name: 'app',
-  data: function() {
-    return {
-      prefixes: ['Air', 'Jet', 'Flight', 'Emirates', 'Tam'],
-      sufixes: ['Hub', 'Station', 'Mart', 'Fly', 'Airlines'],
-      domains: [
-        {
-          name: 'AirHub'
-        },
-        {
-          name: 'AirStation'
-        },
-        {
-          name: 'AirMart'
-        },
-        {
-          name: 'AirFly'
-        },
-        {
-          name: 'AirAirlines'
-        },
-        {
-          name: 'JetHub'
-        },
-        {
-          name: 'JetStation'
-        },
-        {
-          name: 'JetMart'
-        },
-        {
-          name: 'JetFly'
-        },
-        {
-          name: 'JetAirlines'
-        },
-        {
-          name: 'FlightHub'
-        },
-        {
-          name: 'FlightStation'
-        },
-        {
-          name: 'FlightMart'
-        },
-        {
-          name: 'FlightFly'
-        },
-        {
-          name: 'FlightAirlines'
-        },
-        {
-          name: 'EmiratesHub'
-        },
-        {
-          name: 'EmiratesStation'
-        },
-        {
-          name: 'EmiratesMart'
-        },
-        {
-          name: 'EmiratesFly'
-        },
-        {
-          name: 'EmiratesAirlines'
-        },
-        {
-          name: 'TamHub'
-        },
-        {
-          name: 'TamStation'
-        },
-        {
-          name: 'TamMart'
-        },
-        {
-          name: 'TamFly'
-        },
-        {
-          name: 'TamAirlines'
-        }
-      ],
-    };
-  }
-}
+	name: 'app',
+	data: function() {
+		return {
+			prefixes: ['Air', 'Jet', 'Flight', 'Emirates', 'Tam'],
+			sufixes: ['Hub', 'Station', 'Mart', 'Fly', 'Airlines'],
+			domains: [
+				{
+					name: 'AirHub'
+				},
+				{
+					name: 'AirStation'
+				},
+				{
+					name: 'AirMart'
+				},
+				{
+					name: 'AirFly'
+				},
+				{
+					name: 'AirAirlines'
+				},
+				{
+					name: 'JetHub'
+				},
+				{
+					name: 'JetStation'
+				},
+				{
+					name: 'JetMart'
+				},
+				{
+					name: 'JetFly'
+				},
+				{
+					name: 'JetAirlines'
+				},
+				{
+					name: 'FlightHub'
+				},
+				{
+					name: 'FlightStation'
+				},
+				{
+					name: 'FlightMart'
+				},
+				{
+					name: 'FlightFly'
+				},
+				{
+					name: 'FlightAirlines'
+				},
+				{
+					name: 'EmiratesHub'
+				},
+				{
+					name: 'EmiratesStation'
+				},
+				{
+					name: 'EmiratesMart'
+				},
+				{
+					name: 'EmiratesFly'
+				},
+				{
+					name: 'EmiratesAirlines'
+				},
+				{
+					name: 'TamHub'
+				},
+				{
+					name: 'TamStation'
+				},
+				{
+					name: 'TamMart'
+				},
+				{
+					name: 'TamFly'
+				},
+				{
+					name: 'TamAirlines'
+				}
+			],
+		};
+	}
+};
 </script>
 
 <style>
